7c976cef442971fbf835bb48882ab2e1ede4c5c8
Zarina <zarina@gmail.com>
2026-04-01 07:59:56 +0500

    added ArmorHandler class

--- src/com/narxoz/rpg/chain/ArmorHandler.java
+++ src/com/narxoz/rpg/chain/ArmorHandler.java
@@ -1,18 +1,15 @@
 package com.narxoz.rpg.chain;
-
 import com.narxoz.rpg.arena.ArenaFighter;
-
 public class ArmorHandler extends DefenseHandler {
     private final int armorValue;
-
     public ArmorHandler(int armorValue) {
         this.armorValue = armorValue;
     }
-
     @Override
     public void handle(int incomingDamage, ArenaFighter target) {
-        // TODO: Subtract armorValue from incomingDamage; clamp the result to a minimum of 0.
-        // TODO: Print an armor message showing how much was absorbed.
-        // TODO: Pass the remaining damage to the next handler.
+        int absorbed = Math.min(incomingDamage, armorValue);
+        int remainder = Math.max(0, incomingDamage - armorValue);
+        System.out.println("[Armor] Absorbed " + absorbed + " damage");
+        passToNext(remainder, target);
     }
 }
